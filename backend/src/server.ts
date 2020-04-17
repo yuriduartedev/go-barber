091ff6df27fb72ffe0fb921e14c32506139c2ac1
Yuri Duarte <yuriduartedev@gmail.com>
2020-04-17 12:52:06 -0300

    Adiciona modulo global de erros para aplicação

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -11,6 +14,24 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.error(error);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
